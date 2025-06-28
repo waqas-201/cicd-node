abf10147d0a1338213dd1a264356c66994dfdee8
waqas-201 <waqasvu892@gmail.com>
2025-06-29 01:03:36 +0500

    added contact page

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.send("This is the about page.");
 });
+app.get("/contact", (req, res) => {
+  res.send("This is the contact page.");
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
 });
+
